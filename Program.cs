8ba8d16b2f5c921e7695fc5195f33fb2e4471bb8
Jesper Petersen <info@ordklar.dk>
2024-06-22 23:46:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+//String
+string firstFriend = "Jasper";
+Console.WriteLine(firstFriend);
+
+string secondFriend = "Mie";
+string thirdFriend = "Kasper";
+Console.WriteLine($"{secondFriend} are boyfriend with {thirdFriend}");
+
