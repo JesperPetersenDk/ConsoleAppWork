e59c894f21d3d8cfae7357fbe799d0d3a2e23523
Jesper Petersen <info@ordklar.dk>
2024-06-22 23:51:37 +0200

    Tilføj Trim

--- Program.cs
+++ Program.cs
@@ -7,4 +7,11 @@ Console.WriteLine(firstFriend);
 string secondFriend = "Mie";
 string thirdFriend = "Kasper";
 Console.WriteLine($"{secondFriend} are boyfriend with {thirdFriend}");
+//Console.WriteLine(secondFriend + " are boyfriend with " + thirdFriend);
 
+
+//Trim
+string helloWorldTrim = "             Trim text here.              ";
+//Console.WriteLine(helloWorldTrim.TrimStart()); // Trim text here.              
+//Console.WriteLine(helloWorldTrim.TrimEnd()); //              Trim text here.
+Console.WriteLine(helloWorldTrim.Trim());// Trim text here.
